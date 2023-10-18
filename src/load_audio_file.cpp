8f6dd5c1e393080376d4d836f77ac51da0fd65c9
Jules Fouchy <jules.fouchy@ntymail.com>
2023-10-18 20:07:12 +0200

    ⬆️ [RtAudioWrapper]

--- src/load_audio_file.cpp
+++ src/load_audio_file.cpp
@@ -9,11 +9,11 @@ void load_audio_file(RtAudioW::Player& player, std::filesystem::path const& path
     nqr::NyquistIO io;
     nqr::AudioData data;
     io.Load(&data, path.string());
-    player.open(
+    player.set_audio_data({
         std::move(data.samples),
         static_cast<unsigned int>(data.sampleRate),
-        static_cast<unsigned int>(data.channelCount)
-    );
+        static_cast<unsigned int>(data.channelCount),
+    });
 }
 
 } // namespace Cool
