694f0953af4508bf8c7147f348bed2c52d39796b
Jules Fouchy <jules.fouchy@ntymail.com>
2023-11-07 17:48:58 +0100

    💡 Removed TODO

--- src/compute_volume.cpp
+++ src/compute_volume.cpp
@@ -11,7 +11,7 @@ auto compute_volume(std::span<float const> data) -> float
     float sum_of_squares{0.f};
     for (float const sample : data)
         sum_of_squares += sample * sample;
-    return std::sqrt(sum_of_squares / static_cast<float>(data.size())); // TODO(Audio) Return the result in decibels ?
+    return std::sqrt(sum_of_squares / static_cast<float>(data.size()));
 }
 
 } // namespace Audio
