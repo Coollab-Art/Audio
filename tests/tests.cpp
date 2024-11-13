26e90d42b09a4a2ad5c17dbecee8f73eda341cea
Jules Fouchy <jules.fouchy@ntymail.com>
2024-11-13 22:21:15 +0100

    🚨 Fix Windows tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -33,103 +33,104 @@ static auto get_device_name(Audio::InputStream const& input_stream, Audio::Selec
 
 auto main(int argc, char* argv[]) -> int
 {
-    const int  exit_code              = doctest::Context{}.run();                   // Run all unit tests
-    const bool should_run_imgui_tests = argc < 2 || strcmp(argv[1], "-nogpu") != 0; // NOLINT(*pro-bounds-pointer-arithmetic)
-    if (
-        should_run_imgui_tests
-        && exit_code == 0 // Only open the window if the tests passed; this makes it easier to notice when some tests fail
+    int const  exit_code              = doctest::Context{}.run();                   // Run all unit tests
+    bool const should_run_imgui_tests = argc < 2 || strcmp(argv[1], "-nogpu") != 0; // NOLINT(*pro-bounds-pointer-arithmetic)
+    if (!should_run_imgui_tests
+        || exit_code != 0 // Only open the window if the tests passed; this makes it easier to notice when some tests fail
     )
     {
-        // Input stream
-        Audio::InputStream input_stream{
-            [](RtAudioErrorType /* type */, std::string const& error_message) {
-                std::cerr << error_message << '\n';
-            }
-        };
-        static constexpr size_t nb_samples_in_input_stream{512};
-        input_stream.set_nb_of_retained_samples(nb_samples_in_input_stream);
-        // Load the audio file
-        Audio::load_audio_file(Audio::player(), exe_path::dir() / "../../tests/res/Monteverdi - L'Orfeo, Toccata.mp3");
-        Audio::player().play();
-
-        static constexpr int64_t fft_size{8000};
-        float                    max_spectrum_frequency_in_hz{15000.f};
-
-        quick_imgui::loop("Audio tests", [&]() { // Open a window and run all the ImGui-related code
-            auto const spectrum = Audio::fourier_transform(
-                fft_size,
-                [&](std::function<void(float)> const& callback) {
-                    for (int64_t i = 0; i < fft_size; i++)
-                        callback(
-                            window(i, fft_size)
-                            * Audio::player().sample_unaltered_volume(i + Audio::player().current_frame_index())
-                        );
-                },
-                static_cast<float>(Audio::player().audio_data().sample_rate),
-                max_spectrum_frequency_in_hz
-            );
-
-            ImGui::Begin("Audio tests");
-            // Player
-            ImGui::SeparatorText("Player");
-            if (ImGui::Button(Audio::player().properties().is_muted ? "Unmute" : "Mute"))
-                Audio::player().properties().is_muted = !Audio::player().properties().is_muted;
-            ImGui::PlotLines(
-                "Spectrum",
-                spectrum.data.data(),
-                static_cast<int>(spectrum.data.size()),
-                0, nullptr,
-                0.f, 1.f,
-                {0.f, 100.f}
-            );
-            ImGui::SliderFloat("Max spectrum frequency", &max_spectrum_frequency_in_hz, 0.f, 22000.f, "%.0f Hertz");
-
-            // Input stream
-            ImGui::NewLine();
-            ImGui::SeparatorText("Input stream");
-            auto const input_device_ids = input_stream.device_ids();
-            auto const combo_text       = get_device_name(input_stream, input_stream.current_device());
-            if (ImGui::BeginCombo("Input device", combo_text.c_str()))
-            {
-                bool const is_selected = std::holds_alternative<Audio::UseDefaultDevice>(input_stream.current_device());
-                if (ImGui::Selectable(get_device_name_impl(input_stream, Audio::UseDefaultDevice{}).c_str(), is_selected))
-                    input_stream.use_default_device();
-                if (is_selected) // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-                    ImGui::SetItemDefaultFocus();
-
-                for (auto const id : input_device_ids)
-                {
-                    auto const info         = input_stream.device_info(id);
-                    bool const is_selected2 = info.name == combo_text;
-                    if (ImGui::Selectable(info.name.c_str(), is_selected2))
-                    {
-                        input_stream.use_given_device(info);
-                    }
-
-                    if (is_selected2) // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-                        ImGui::SetItemDefaultFocus();
-                }
-                ImGui::EndCombo();
-            }
-            auto data_from_input_stream = std::vector<float>{};
-            input_stream.for_each_sample(nb_samples_in_input_stream, [&](float const sample) {
-                data_from_input_stream.push_back(sample);
-            });
-            ImGui::PlotLines(
-                "Waveform",
-                data_from_input_stream.data(),
-                static_cast<int>(data_from_input_stream.size()),
-                0, nullptr,
-                -0.1f, 0.1f,
-                {0.f, 100.f}
-            );
-            //
-            ImGui::End();
-            ImGui::ShowDemoWindow();
-        });
+        return exit_code;
     }
+
+    // Input stream
+    Audio::InputStream input_stream{
+        [](RtAudioErrorType /* type */, std::string const& error_message) {
+            std::cerr << error_message << '\n';
+        }
+    };
+    static constexpr size_t nb_samples_in_input_stream{512};
+    input_stream.set_nb_of_retained_samples(nb_samples_in_input_stream);
+    // Load the audio file
+    Audio::load_audio_file(Audio::player(), exe_path::dir() / "../../tests/res/Monteverdi - L'Orfeo, Toccata.mp3");
+    Audio::player().play();
+
+    static constexpr int64_t fft_size{8000};
+    float                    max_spectrum_frequency_in_hz{15000.f};
+
+    quick_imgui::loop("Audio tests", [&]() { // Open a window and run all the ImGui-related code
+        auto const spectrum = Audio::fourier_transform(
+            fft_size,
+            [&](std::function<void(float)> const& callback) {
+                for (int64_t i = 0; i < fft_size; i++)
+                    callback(
+                        window(i, fft_size)
+                        * Audio::player().sample_unaltered_volume(i + Audio::player().current_frame_index())
+                    );
+            },
+            static_cast<float>(Audio::player().audio_data().sample_rate),
+            max_spectrum_frequency_in_hz
+        );
+
+        ImGui::Begin("Audio tests");
+        // Player
+        ImGui::SeparatorText("Player");
+        if (ImGui::Button(Audio::player().properties().is_muted ? "Unmute" : "Mute"))
+            Audio::player().properties().is_muted = !Audio::player().properties().is_muted;
+        ImGui::PlotLines(
+            "Spectrum",
+            spectrum.data.data(),
+            static_cast<int>(spectrum.data.size()),
+            0, nullptr,
+            0.f, 1.f,
+            {0.f, 100.f}
+        );
+        ImGui::SliderFloat("Max spectrum frequency", &max_spectrum_frequency_in_hz, 0.f, 22000.f, "%.0f Hertz");
+
+        // Input stream
+        ImGui::NewLine();
+        ImGui::SeparatorText("Input stream");
+        auto const input_device_ids = input_stream.device_ids();
+        auto const combo_text       = get_device_name(input_stream, input_stream.current_device());
+        if (ImGui::BeginCombo("Input device", combo_text.c_str()))
+        {
+            bool const is_selected = std::holds_alternative<Audio::UseDefaultDevice>(input_stream.current_device());
+            if (ImGui::Selectable(get_device_name_impl(input_stream, Audio::UseDefaultDevice{}).c_str(), is_selected))
+                input_stream.use_default_device();
+            if (is_selected) // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                ImGui::SetItemDefaultFocus();
+
+            for (auto const id : input_device_ids)
+            {
+                auto const info         = input_stream.device_info(id);
+                bool const is_selected2 = info.name == combo_text;
+                if (ImGui::Selectable(info.name.c_str(), is_selected2))
+                {
+                    input_stream.use_given_device(info);
+                }
+
+                if (is_selected2) // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                    ImGui::SetItemDefaultFocus();
+            }
+            ImGui::EndCombo();
+        }
+        auto data_from_input_stream = std::vector<float>{};
+        input_stream.for_each_sample(nb_samples_in_input_stream, [&](float const sample) {
+            data_from_input_stream.push_back(sample);
+        });
+        ImGui::PlotLines(
+            "Waveform",
+            data_from_input_stream.data(),
+            static_cast<int>(data_from_input_stream.size()),
+            0, nullptr,
+            -0.1f, 0.1f,
+            {0.f, 100.f}
+        );
+        //
+        ImGui::End();
+        ImGui::ShowDemoWindow();
+    });
     Audio::shut_down();
-    return exit_code;
+    return 0;
 }
 
 TEST_CASE("At least one API has been compiled")
